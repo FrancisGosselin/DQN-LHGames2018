4e397676ff6995dab770e2b362b55695d6b50004
FrancisGosselin <francis.gosselin@polymtl.ca>
2018-10-07 00:32:47 -0400

    added all files

---
+++ README.txt
@@ -0,0 +1,20 @@
+rewards:
+- do undoable action: -1
+- lose hp: -5
+- die: -100
+- kill: +20
+- attack: +5
+- collect ressouces: +5
+- destroy wall : +10
+
+
+action vector:
+0: go left
+1: go right
+2: go up
+3: go down
+4: attack
+5: collect
+6: purchase
+7: upgrade 
+8: drink potion
---
+++ bot/bot.py
@@ -0,0 +1,37 @@
+from helper import *
+from bot.agent import *
+
+class Bot:
+    def __init__(self):
+        self.initialize = True
+        self.agent = Agent()
+        pass
+
+    def before_turn(self, playerInfo):
+        """
+        Gets called before ExecuteTurn. This is where you get your bot's state.
+            :param playerInfo: Your bot's current state.
+        """
+        if (self.initialize):
+            self.agent.setPrevInfo(playerInfo)
+            self.initialize = False
+        self.agent.setNextInfo(playerInfo)
+        self.PlayerInfo = playerInfo
+
+    def execute_turn(self, gameMap, visiblePlayers):
+        """
+        This is where you decide what action to take.
+            :param gameMap: The gamemap.
+            :param visiblePlayers:  The list of visible players.
+        """
+        action = self.agent.action(gameMap, visiblePlayers)
+        # Write your bot here. Use functions from aiHelper to instantiate your actions.
+        return action
+
+    def after_turn(self):
+        """
+        Gets called after executeTurn
+        """
+        pass
+    def getState():
+        return self.state
